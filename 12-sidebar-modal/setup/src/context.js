494939856f048ace62360aa5a039c629cf84c169
Anitor <anitorabraham@gmail.com>
2022-02-12 16:36:00 +0100

    sidebar completed

--- 12-sidebar-modal/setup/src/context.js
+++ 12-sidebar-modal/setup/src/context.js
@@ -1,1 +1,49 @@
-import React, { useState, useContext } from 'react'
+import React, { useState, useContext } from "react";
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  // this where all our functions and state are located
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    // dont forget your children
+    <AppContext.Provider
+      value={{
+        isModalOpen,
+        isSidebarOpen,
+        openModal,
+        closeModal,
+        closeSidebar,
+        openSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+// custom Hook
+// this is to prevent numerous import when we are accessing our state and functions
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
--- 12-sidebar-modal/setup/src/index.js
+++ 12-sidebar-modal/setup/src/index.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { AppProvider } from "./context";
+import App from "./App";
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* This is where we put in our AppProvider with the children as our <App /> */}
+    <AppProvider>
+      <App />
+    </AppProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
